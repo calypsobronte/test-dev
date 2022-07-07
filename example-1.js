1d47d5334b7b0364a21806d6283df55d4d0dea24
Lina María Montaño Ramírez - @calypsobronte <codelinamaria@gmail.com>
2022-07-07 13:30:52 -0500

    Crear una función tomando la variable companies como parámetro

--- example-1.js
+++ example-1.js
@@ -3,7 +3,28 @@ const companies = createAll();
 
 cleanConsole(1, companies);
 
-console.log("---- EXAMPLE 1 --- ", companies);
+
+// TODO: Crear una función tomando la variable "companies" como parámetro  
+export const exampleOne = (companies) => {
+    return companies.map(company => {
+        // El nombre de cada "company" debe tener una letra mayúscula al principio
+        company.name = company.name.charAt(0).toUpperCase() + company.name.slice(1)
+        company.users = company.users.map(user => {
+            // reemplazando todos los valores "undefined" en "usuarios" por un string vacío.
+            user.firstName === undefined
+                ? (user.firstName = "") // El nombre y apellifo de cada "user" debe tener una letra mayúscula al principio
+                : user.firstName = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+            user.lastName === undefined
+                ? (user.lastName = "")
+                : user.lastName = user.lastName.charAt(0).toUpperCase() + user.lastName.slice(1);
+            return user;
+        }).sort((a, b) => a.firstName.localeCompare(b.firstName)); // Los "users" de cada "company" deben aparecer en orden alfabético.
+
+        return company
+    }).sort((a, b) => b.usersLength - a.usersLength); // // Las "companies" deben ordenarse por su total de "user" (orden decreciente)
+}
+
+console.log("---- EXAMPLE 1 --- ", exampleOne(companies));
 
 // -----------------------------------------------------------------------------
 // INSTRUCCIONES EN ESPAÑOL
