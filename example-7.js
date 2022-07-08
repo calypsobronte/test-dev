66de984fdfd6bf35307974ae099429035347a138
Lina María Montaño Ramírez - @calypsobronte <codelinamaria@gmail.com>
2022-07-07 21:41:31 -0500

    ---- EXAMPLE 7 diferentes partes 1 al 9 ---

--- example-7.js
+++ example-7.js
@@ -1,29 +1,39 @@
-import {cleanConsole, createAll} from './data';
+import { cleanConsole, createAll } from './data';
 
 const companies = createAll();
 
-cleanConsole(7, companies);
-console.log('---- EXAMPLE 7 part 1 --- ', companies);
-console.log('---- EXAMPLE 7 part 2 --- ', companies);
-console.log('---- EXAMPLE 7 part 3 --- ', companies);
-console.log('---- EXAMPLE 7 part 4 --- ', companies);
-console.log('---- EXAMPLE 7 part 5 --- ', companies);
-console.log('---- EXAMPLE 7 part 6 --- ', companies);
-console.log('---- EXAMPLE 7 part 7 --- ', companies);
-console.log('---- EXAMPLE 7 part 8 --- ', companies);
-console.log('---- EXAMPLE 7 part 9 --- ', companies);
 // -----------------------------------------------------------------------------
 // INSTRUCCIONES EN ESPAÑOL
 
 // Parte 1: Crear una función tomando como parámetro un "id" de "company" y
 // devolviendo el nombre de esta "company".
 
+export const companyById = (id) => {
+    return companies.find(company => company.id === id).name;
+}
+
 // Parte 2: Crear una función tomando como parámetro un "id" de "company" y
 // quitando la "company" de la lista.
+export const companyDeleteById = (id) => {
+    return companies.splice(companies.findIndex(company => company.id === id), 1);
+}
 
 // Parte 3: Crear una función tomando como parámetro un "id" de "company" y
 // permitiendo hacer un PATCH (como con una llamada HTTP) en todos los
 // atributos de esta "company" excepto en el atributo "users".
+
+export const companyPatch = (id, payload) => {
+    const company = companies.filter((item) => item.id === id);
+    if (company) {
+        const indice = companies.findIndex((item) => item.id == id);
+        Object.keys(payload).map((item) => {
+            if (item !== 'users') {
+                companies[indice][item] = payload[item];
+            }
+        });
+        return companies[indice];
+    }
+}
 
 // Parte 4: Crear una función tomando como parámetro un "id" de "company" y un
 // nuevo "user" cuyo el apelido es "Delgado", el nombre "Juan", de 35 años y
@@ -31,26 +41,112 @@ console.log('---- EXAMPLE 7 part 9 --- ', companies);
 // "company" y tener un "id" generado automáticamente. La función también debe modificar
 // el atributo "usersLength" de "company".
 
+export const addUserToCompany = (id, payload) => {
+    const company = companies.find((item) => item.id === id);
+    if (company) {
+        company.users.push({ ...payload, id: Math.random() });
+        company.usersLength = company.users.length;
+    }
+    return company
+}
+
 // Parte 5: Crear una función tomando como parámetro un "id" de "company" y
 // permitiendo hacer un PUT (como con una llamada HTTP) en esta "company" excepto
 // en el atributo "users".
 
+export const companyPut = (id, payload) => {
+    const aux = companies
+    const company = aux.filter((item) => item.id === id);
+    if (company) {
+        const indice = aux.findIndex((item) => item.id == id);
+        aux[indice] = { ...aux[indice], ...payload, users: aux[indice].users };
+        return aux[indice];
+    }
+}
+
 // Parte 6: Crear una función tomando como parámetro un "id" de "company" y un
 // "id" de "user". La función debe quitar este "user" de la lista de "users"
 // de "company" y cambiar el atributo "usersLength" de "company".
+export const deleteUserToCompany = (idCompany, idUser) => {
+    const aux = companies;
+    const company = aux.findIndex((company) => company.id === idCompany);
+    const user = aux[company].users.findIndex((user) => user.id === idUser);
+    aux[company].users.splice(user, 1);
+    aux[company].usersLength--;
+    return aux;
+}
+
 
 // Parte 7: Crear una función tomando como parámetro un "id" de "company" y un
 // "id" de "user" que permite hacer un PATCH (como con una llamada HTTP) en este
 // "user".
+export const userPatchById = (idCompany, idUser, payload) => {
+    const company = companies.filter((item) => item.id === idCompany);
+    const aux = companies
+    if (company) {
+        const indiceCompany = aux.findIndex((item) => item.id == idCompany);
+        const indiceUser = aux[indiceCompany].users.findIndex((item) => item.id == idUser);
+        Object.keys(payload).map((item) => {
+            companies[indiceCompany].users[indiceUser][item] = payload[item];
+        });
+
+        return aux;
+    }
+}
 
 // Parte 8: Crear una función tomando como parámetro un "id" de "company" y un
 // "id" de "user" que permite hacer un PUT (como con una llamada HTTP) en este
 // "user".
+export const userPutById = (idCompany, idUser, payload) => {
+    const company = companies.filter((item) => item.id === idCompany);
+    const aux = companies
+    if (company) {
+        const indiceCompany = aux.findIndex((item) => item.id == idCompany);
+        const indiceUser = aux[indiceCompany].users.findIndex((item) => item.id == idUser);
+        if (indiceUser !== -1) {
+            aux[indiceCompany].users[indiceUser] = { ...aux[indiceCompany].users[indiceUser], ...payload };
+        }
+        return aux;
+    }
+}
 
 // Parte 9: Crear una función tomando como parámetro dos "id" de "company" y
 // un "id" de "user". La función debe permitir que el user sea transferido de la
 // primera "company" a la segunda "company". El atributo "usersLength" de cada
 // "company" debe actualizarse.
+export const transferidoUsuario = (idCompanyOne, idCompanyTwo, idUser) => {
+    const company = companies.filter((item) => item.id === idCompanyOne);
+    const aux = companies
+    if (company) {
+        const companyOne = aux.find((company) => company.id === idCompanyOne);
+
+        const user = companyOne.users.find((user) => user.id === idUser);
+
+        if (user) {
+            aux = deleteUserToCompany(idCompanyOne, idUser);
+            aux = addUserToCompany(idCompanyTwo, user);
+        }
+        return aux;
+    }
+}
+
+cleanConsole(7, companies);
+console.log('---- EXAMPLE 7 part 1 --- ', companies, companyById(7));
+console.log('---- EXAMPLE 7 part 2 --- ', companies, companyDeleteById(7));
+console.log('---- EXAMPLE 7 part 3 --- ', companies, companyPatch(3, { name: 'Nike-Air', users: [] }));
+console.log('---- EXAMPLE 7 part 4 --- ', companies, addUserToCompany(1, { firstName: "Juan", lastName: "Delgado", age: 35, car: true }));
+console.log('---- EXAMPLE 7 part 5 --- ', companies, companyPut(3, { id: 200, users: [{ name: 'Juan' }] }));
+console.log('---- EXAMPLE 7 part 6 --- ', companies, deleteUserToCompany(6, 5));
+console.log('---- EXAMPLE 7 part 7 --- ', companies, userPatchById(1, 9, { lastName: 'Montaño' }));
+console.log('---- EXAMPLE 7 part 8 --- ', companies, userPutById(5, 12, { lastName: 'Dolar' }));
+console.log('---- EXAMPLE 7 part 9 --- ', companies, transferidoUsuario(5, 1, 12));
+
+
+
+
+
+
+
 
 // -----------------------------------------------------------------------------
 // INSTRUCTIONS IN ENGLISH
